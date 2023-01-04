74daf47d3486a6f6dcb343bd13c3186a131c6179
mj444555 <106246063+Mahi444555@users.noreply.github.com>
2023-01-04 22:14:53 +0530

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 ////////middleware
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","https://backend-updated.onrender.com");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
